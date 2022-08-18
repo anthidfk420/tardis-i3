5e2db5cd51cefe8aa40038cd6ee38f40cfd15ddd
anthidfk420 <73663954+anthidfk420@users.noreply.github.com>
2022-08-18 18:50:16 +0100

    Hopefully this works (probably not)

--- new-script.sh
+++ new-script.sh
@@ -6,10 +6,14 @@ launch_script () {
 
 internet_check () {
 	homeuser=$USER
-	echo "Welcome to TARDIS, a utility for Arch Linux that automatically installs and configures a graphical environment based on i3wm." 
-	sleep 2
-	echo && echo "All of the dotfiles are included in this script, but avoid modifying them until the install is complete."
-	echo && sleep 2
+
+  echo '  _____ _   ___ ___ ___ ___     _ ____'
+ echo ' |_   _/_\ | _ \   \_ _/ __|___(_)__ /'
+   echo '   | |/ _ \|   / |) | |\__ \___| ||_ \'
+   echo '   |_/_/ \_\_|_\___/___|___/   |_|___/'
+	echo 
+	echo "Free Software licensed under the GNU General Public License 3.0"	
+   echo && sleep 2	
 	read -p "Can you confirm you're connected to the internet and want to go ahead with the installer? [y/n] " internet
 	if [ $internet == 'y' ] || [ $internet == 'Y' ]
 	then
@@ -41,7 +45,7 @@ pacman_update () {
 }
 
 aur_setup () {
-       	# AUR setup is being skipped because makepkg cannot be run as root.
+	pacman -S --needed git base-devel && git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si
 	pacman_install
 }
 
@@ -54,16 +58,25 @@ end_script () {
 
 # Installing packages
 pacman_install () {
-	pacman -S i3 dmenu polybar ttf-fira-code thunar materia-gtk-theme ttf-fira-sans sddm noto-fonts noto-fonts-cjk nitrogen wireplumber pipewire pipewire-pulse pavucontrol xfce4-terminal network-manager-applet chromium vim --noconfirm --needed
+	pacman -S i3 dmenu polybar ttf-fira-code arc-icon-theme lxappearance thunar materia-gtk-theme ttf-fira-sans sddm noto-fonts noto-fonts-cjk nitrogen wireplumber pipewire pipewire-pulse pavucontrol xfce4-terminal network-manager-applet chromium vim --noconfirm --needed
 	systemctl enable sddm
 	wm_customise
 }
 
 # Setup for WM and utilities
 wm_customise () {
+	rm -rf /home/$homeuser/.config
 	mkdir -p /home/$homeuser/.config/i3 && mkdir -p /home/$homeuser/.config/polybar
 	cp -r config/i3/config /home/$homeuser/.config/i3
 	cp -r config/polybar/config.ini /home/$homeuser/.config/polybar
+	cp -r 'config/gtk-2.0' /home/$homeuser/.config
+	cp -r 'config/gtk-3.0' /home/$homeuser/.config
+	end_of_install
+}
+
+end_of_install () {
+	clear
+	echo "Installation complete, please restart your computer."
 }
 
 # What causes the script to run
